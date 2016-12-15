bfd59803f6abe895a08d4d6c6930ed75afd0c960
Kamal <kamalakar.sambaraj@gmai.com>
2016-12-15 16:06:18 +0530

    Starting Freshly to deal with image upload!

--- app/src/main/java/com/hkapps/storykorner/ProfileFragment.java
+++ app/src/main/java/com/hkapps/storykorner/ProfileFragment.java
@@ -1,26 +1,16 @@
 package com.hkapps.storykorner;
 
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -47,50 +37,11 @@ public class ProfileFragment extends Fragment {
         uname.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
 
 
-        prof_image = (ImageButton) rootview.findViewById(R.id.prof_image);
-        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        prof_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, GALLERY_INTENT);
-            }
-        });
 
 
         return rootview;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-
-        if (requestCode == GALLERY_INTENT && requestCode == RESULT_OK) {
-
-            Toast.makeText(getContext(), "Uploading started", Toast.LENGTH_SHORT).show();
-
-            Uri uri = data.getData();
-
-            StorageReference filepath = mStorageRef.child("User_photos").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    Toast.makeText(getContext(), "Done Uploading...", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            filepath.putFile(uri).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getContext(), "Failed to Upload...", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-        }
-    }
 }
