1ab11d680ce7f0016b6bb05e21fc4804ad96e58a
Kamal <kamalakar.sambaraj@gmai.com>
2016-12-24 00:05:12 +0530

    Removed toolbar.... added colorPrimaryDark... Profile Screen looking Pretty Good...!

--- app/src/main/java/com/hkapps/storykorner/MainActivity.java
+++ app/src/main/java/com/hkapps/storykorner/MainActivity.java
@@ -2,7 +2,6 @@ package com.hkapps.storykorner;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -11,7 +10,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -39,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
         fragmentManager = getSupportFragmentManager();
