04f4a22e76325bead6d407b84e5158ccb99f808d
Kamal <kamalakar.sambaraj@gmai.com>
2017-01-05 01:32:07 +0530

    Search - 1 Screen Implementation

--- app/src/main/java/com/hkapps/storykorner/SearchFragment.java
+++ app/src/main/java/com/hkapps/storykorner/SearchFragment.java
@@ -3,9 +3,13 @@ package com.hkapps.storykorner;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 /**
@@ -24,6 +28,50 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_search, container, false);
+
+
+        LinearLayout search_parent = (LinearLayout) rootview.findViewById(R.id.search_parent);
+
+
+        search_parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "Hi", Toast.LENGTH_SHORT).show();
+                Fragment fragment = new SearchUserFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+
+            }
+        });
+
+       /*
+
+        sv1.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Hi", Toast.LENGTH_SHORT).show();
+                Fragment fragment = new SearchUserFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+
+            }
+        });*/
 
 
         return rootview;
