2ffe78feb23132a876585af593c800fa7b820f77
Harish111 <gurramharish275@gmail.com>
2017-01-05 13:17:45 +0530

    Implemented UI On clicking on categories it takes to storiesfragment screen and shows a Toast

--- app/src/main/java/com/hkapps/storykorner/StoriesFragment.java
+++ app/src/main/java/com/hkapps/storykorner/StoriesFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -51,6 +52,20 @@ public class StoriesFragment extends Fragment {
         sharedPreference = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String storyuserid = sharedPreference.getString("storyuserid", null);
         boolean chk = sharedPreference.getBoolean("profile", false);
+
+
+
+
+        String Category = sharedPreference.getString("Category", "Humor");
+        Boolean CategoryBoolean = sharedPreference.getBoolean("CategoryBoolean", false);
+
+
+
+        if(CategoryBoolean.equals(true)){
+
+            Toast.makeText(getContext(), Category, Toast.LENGTH_SHORT).show();
+        }
+
 
         linearLayoutManager = new LinearLayoutManager(getActivity());
         storyRecyclerview = (RecyclerView) rootview.findViewById(R.id.story_recycler_view);
