2730ad23c2e418a31b13d844055602fd45541078
Kamal <kamalakar.sambaraj@gmai.com>
2017-01-10 15:50:56 +0530

    onclick notifiactions list open profile of the liked user

--- app/src/main/java/com/hkapps/storykorner/NotifyHolder.java
+++ app/src/main/java/com/hkapps/storykorner/NotifyHolder.java
@@ -12,11 +12,14 @@ public class NotifyHolder extends RecyclerView.ViewHolder {
 
     public TextView liker_name;
     public ImageView notify_imgview;
+    public TextView notify_time;
+    public ImageView imgview;
 
     public NotifyHolder(View itemView) {
         super(itemView);
 
         liker_name = (TextView) itemView.findViewById(R.id.liker_name);
         notify_imgview = (ImageView) itemView.findViewById(R.id.notify_imgview);
+        notify_time = (TextView) itemView.findViewById(R.id.notify_time);
     }
 }
