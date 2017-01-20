8857dbae02ff64e1098474e8d784e3e123eb751e
Kamal <kamalakar.sambaraj@gmai.com>
2017-01-21 00:59:38 +0530

    Added photo to notification

--- app/src/main/java/com/hkapps/storykorner/NotificationListener.java
+++ app/src/main/java/com/hkapps/storykorner/NotificationListener.java
@@ -6,6 +6,9 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -18,6 +21,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 /**
  * Created by kamal on 23-12-2016.
@@ -27,6 +32,8 @@ public class NotificationListener extends Service {
 
     public static boolean isRunning;
     private DatabaseReference notifying;
+    private Target mTarget;
+    private NotificationCompat.Builder mBuilder;
 
     @Nullable
     @Override
@@ -54,10 +61,10 @@ public class NotificationListener extends Service {
         notifying.keepSynced(true);
         notifying.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            public void onChildAdded(final DataSnapshot dataSnapshot1, String s) {
 
-                String photo = null;
-                String uid = dataSnapshot.child("liker_id").getValue().toString();
+
+                String uid = dataSnapshot1.child("liker_id").getValue().toString();
 
                 DatabaseReference imgRef = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
 
@@ -67,7 +74,13 @@ public class NotificationListener extends Service {
                         if (dataSnapshot.child("photolink").exists()) {
                             String photo = dataSnapshot.child("photolink").getValue().toString();
                             // Picasso.with(context).load(photo).fit().centerCrop().into(viewHolder.notify_imgview);
+                            showNotifications(dataSnapshot1.child("liker_name").getValue().toString(), photo);
+                        } else {
+                            showNotifications(dataSnapshot1.child("liker_name").getValue().toString(), null);
+
                         }
+
+
                     }
 
                     @Override
@@ -77,7 +90,6 @@ public class NotificationListener extends Service {
                 });
 
 
-                showNotifications(dataSnapshot.child("liker_name").getValue().toString(), photo);
 
 
             }
@@ -172,33 +184,27 @@ public class NotificationListener extends Service {
 
     private void showNotifications(String username, String pic) {
 
-        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder = new NotificationCompat.Builder(this);
 
 
         // mBuilder.setLargeIcon(Picasso.with(getBaseContext()).load(pic).get());
 
         mBuilder.setContentTitle(username);
         mBuilder.setContentText(username + " Liked your post");
-        mBuilder.setSmallIcon(R.drawable.profile_bg_min);
+        mBuilder.setSmallIcon(R.drawable.ic_notifications_black_24dp);
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_account_circle_black_24dp);
+        mBuilder.setLargeIcon(icon);
+        //  Toast.makeText(this, pic, Toast.LENGTH_SHORT).show();
+        loadImage(getApplicationContext(), pic);
+
+     /*   if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mBuilder.setSmallIcon(R.drawable.bestfrnds);
+        } else {
+           mBuilder.setSmallIcon(R.drawable.bestfrnds);
+        }*/
 
 
-
-     /*   Picasso.with(getApplicationContext()).load(pic).into(new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-
-                mBuilder.setLargeIcon(bitmap);
-            }
-
-            @Override
-            public void onBitmapFailed(Drawable errorDrawable) {
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-            }
-        });
-*/
 
 
 
@@ -239,5 +245,34 @@ public class NotificationListener extends Service {
 */
 
 
+
     }
+
+
+    void loadImage(Context context, String url) {
+
+
+        mTarget = new Target() {
+            @Override
+            public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+                //Do something
+                mBuilder.setLargeIcon(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        Picasso.with(context)
+                .load(url)
+                .into(mTarget);
+    }
+
 }
